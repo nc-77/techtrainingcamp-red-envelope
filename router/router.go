0ec800bb858a14e801d5194751f102087397ed85
nc-77 <291993554@qq.com>
2021-11-02 12:27:31 +0800

    feat: init api

---
+++ router/router.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"red_packet/api"
+)
+
+func InitRouter() *fiber.App {
+	router := fiber.New()
+
+	v0 := router.Group("/v0")
+	v0.Use(cors.New(), logger.New())
+	v0.Post("/snatch", api.Snatch)
+	v0.Post("/open", api.Open)
+	v0.Post("/get_wallet_list", api.GetWalletList)
+
+	return router
+}
---
+++ utils/utils.go
@@ -0,0 +1,10 @@
+package utils
+
+import "os"
+
+func GetEnv(env string, defaultVal string) (key string) {
+	if key = os.Getenv(env); key == "" {
+		key = defaultVal
+	}
+	return
+}
